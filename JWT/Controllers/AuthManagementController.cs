e3b4618f3003c541c08324b4e7751abcc2eabe9c
Vithesh U S <Vithesh.US@novigosolutions.com>
2024-10-31 10:46:34 +0530

    Jwt

---
+++ JWT/Controllers/AuthManagementController.cs
@@ -0,0 +1,83 @@
+﻿using JWT.Configurations;
+using JWT.Models.Dto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthManagementController:ControllerBase
+    {
+        private readonly ILogger<AuthManagementController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+        public AuthManagementController(
+            ILogger<AuthManagementController> logger, 
+            UserManager<IdentityUser> userManager,
+            IOptionsMonitor<JwtConfig> optionsMonitor
+            )
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _jwtConfig = optionsMonitor.CurrentValue;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody]UserRegistrationRequestDto userRegistrationRequestDto)
+        {
+           if(!ModelState.IsValid)
+                return BadRequest("Invalid Request");
+
+            var existingUser =await _userManager.FindByEmailAsync(userRegistrationRequestDto.Email);
+
+            if(existingUser != null)
+                return BadRequest("Email already exists");
+
+            var newUser = new IdentityUser
+            {
+                UserName=userRegistrationRequestDto.Name,
+                Email= userRegistrationRequestDto.Email,
+            };
+            var isCreated=await _userManager.CreateAsync(newUser,userRegistrationRequestDto.Password);
+
+            if (isCreated.Succeeded)
+            {
+                return Ok(new RegistrationResponse
+                {
+                    Result=true,
+                    Token=""
+                });
+            }
+            else
+                return BadRequest("Error while creating the user");
+        }
+        //private string GenerateJwtToken(IdentityUser User)
+        //{
+        //    var jwtTokenHandler=new JwtSecurityTokenHandler();
+
+        //    var key=Encoding.ASCII.GetBytes(_jwtConfig.Secret);
+
+        //    var tokenDescriptor = new SecurityTokenDescriptor
+        //    {
+        //        Subject=new System.Security.Claims.ClaimsIdentity(new []
+        //        {
+        //            new Claim("Id",User.Id),
+        //            new Claim(JwtRegisteredClaimNames.Sub,User.Email!),
+        //            new Claim(JwtRegisteredClaimNames.Email,User.Email!),
+        //            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        //        }),
+        //        Expires=DateTime.Now.AddDays(1),
+        //        SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512)
+        //    };
+        //}
+
+    }
+}
--- JWT/Program.cs
+++ JWT/Program.cs
@@ -2,10 +2,10 @@ using JWT.Configurations;
 using JWT.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +20,33 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JWTSetting:Secret").Value!);
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, //makes sure that the token is signed with the the signinkey
+        IssuerSigningKey = new SymmetricSecurityKey(key), //sets the signing key
+        ValidateIssuer = true, //check the token's issuer if it matches and expected value
+        ValidateAudience = false,//using this we can make sure that a specific api will recieve a specific token. Its just making sure that the receipient is correct
+        RequireExpirationTime=false, //the token will never expire
+        ValidateLifetime = false //if false the expired tokens will still be accepted
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
